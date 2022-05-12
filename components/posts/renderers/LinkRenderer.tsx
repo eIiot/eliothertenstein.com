f61b379fb6bf8cd84f94ffe43cc6208028a126fa
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-05-11 19:29:14 -0700

    improve styling on post details
    - move link to tailwindcss
    - improve border/padding

--- components/posts/renderers/LinkRenderer.tsx
+++ components/posts/renderers/LinkRenderer.tsx
@@ -3,28 +3,30 @@
 /* eslint-disable react/prop-types */
 import { RenderFn } from 'editorjs-blocks-react-renderer'
 
-const LinkRenderer: RenderFn<{}> = ({ data, className = '' }) => {
+const LinkRenderer: RenderFn<{}> = ({ data }) => {
   return (
     <a
-      className="Container border-neutral-600 bg-white"
+      className="flex h-fit flex-col rounded-lg border-neutral-600 bg-white text-left text-black transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-neutral-100"
       data-testid="container"
       href={data.link}
       rel="noreferrer"
       target="_blank"
     >
       <div
-        className="Image"
+        className="h-[30vh] w-full rounded-t-lg bg-cover bg-center bg-no-repeat"
         data-testid="image-container"
         style={{
           backgroundImage: `url("${data.meta.image}"), url("https://i.imgur.com/UeDNBNQ.jpeg")`,
         }}
       />
-      <div className="LowerContainer">
-        <h3 className="Title text-black" data-testid="title">
+      <div className="p-3">
+        <h3 className="mt-0 mb-3 text-black" data-testid="title">
           {data.meta.title}
         </h3>
-        <div className="Description text-black">{data.meta.description}</div>
-        <div className="Secondary SiteDetails text-gray-500">
+        <div className="hidden text-black lg:block">
+          {data.meta.description}
+        </div>
+        <div className=" text-neutral-500">
           {data.link.replace(/^https?:\/\//, '').replace(/\/$/, '')}
         </div>
       </div>
