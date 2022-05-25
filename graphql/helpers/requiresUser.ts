79bb10e2c5659197005bce1ff4b232fda6e912de
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-05-24 18:18:52 -0700

    vscode is bad but phpstorm is worse

--- graphql/helpers/requiresUser.ts
+++ graphql/helpers/requiresUser.ts
@@ -1,8 +1,8 @@
-import { RawContext } from '../context'
+import { Context } from '../context'
 import { AuthenticationError } from 'apollo-server-nextjs'
 
 export function requiresUser(fn: any) {
-  return function resolve(parent: any, args: any, context: RawContext) {
+  return function resolve(parent: any, args: any, context: Context) {
     if (context.viewer) {
       return fn(parent, args, context)
     }
