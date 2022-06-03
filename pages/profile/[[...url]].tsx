76cc06623d5acdd830f161fb7fd3156aac1212c3
Eliot Hertenstein <39346975+eIiot@users.noreply.github.com>
2022-06-02 23:39:33 -0700

    Next SEO

--- pages/profile/[[...url]].tsx
+++ pages/profile/[[...url]].tsx
@@ -1,11 +1,21 @@
+import { NextSeo } from 'next-seo'
 import { User } from 'react-feather'
 
 const profileCatchAll = () => {
   return (
-    <div className="inset-0 flex h-full flex-col items-center justify-center">
-      <User size={64} />
-      <h1>Profiles coming soon™️</h1>
-    </div>
+    <>
+      <NextSeo
+        description="Profiles coming soon!"
+        robotsProps={{
+          noarchive: true,
+        }}
+        title="User Profile"
+      />
+      <div className="inset-0 flex h-full flex-col items-center justify-center">
+        <User size={64} />
+        <h1>Profiles coming soon™️</h1>
+      </div>
+    </>
   )
 }
 
